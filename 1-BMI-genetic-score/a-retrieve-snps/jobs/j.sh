1a9e05c48215eece82b5384a6fbe09b6da260bc7
Louise Millard <louise.millard@bristol.ac.uk>
2018-09-11 12:04:33 +0100

    retrieve snps jobs changed to job array

--- 1-BMI-genetic-score/a-retrieve-snps/jobs/j.sh
+++ 1-BMI-genetic-score/a-retrieve-snps/jobs/j.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #PBS -l walltime=4:00:00,nodes=1:ppn=16
-#PBS -o output-snpsCHR.file
+#PBS -o output
+#PBS -e errors
+#PBS -t 1-22
 #---------------------------------------------
 
 date
@@ -9,13 +11,13 @@ module load languages/gcc-5.0
 module load libraries/gnu_builds/gsl-1.16
 module load apps/qctool-2.0
 
-chr=$(printf "%02d" CHR)
+chr=$(printf "%02d" ${PBS_ARRAYID})
 
 cd $PBS_O_WORKDIR
 
 dir="${HOME}/2016-biobank-mr-phewas-bmi/data/sample500/snps/"
 ddir="${UKB_DATA}/_latest/UKBIOBANK_Array_Genotypes_500k_HRC_Imputation/data/"
-datadir="${ddir}dosage_bgen/"
+datadir="${ddir}raw_downloaded/dosage_bgen/"
 
 sampleFile="${ddir}sample-stats/data.chr${chr}.sample"
 
--- 1-BMI-genetic-score/a-retrieve-snps/jobs/runjobs.sh
+++
@@ -1,7 +0,0 @@
-
-
-for i in $(seq 1 1 22)
-do 
-	qsub j${i}.sh
-	sleep 3
-done
